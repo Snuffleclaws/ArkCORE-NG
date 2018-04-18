d20ea32853165541c42bfcd67128e752cba4ae5c
Steve <38110702+Snuffleclaws@users.noreply.github.com>
2018-04-18 11:03:06 -0400

    Update 2018_04_18_02_world.sql

--- sql/updates/world/2018_04_18_02_world.sql
+++ sql/updates/world/2018_04_18_02_world.sql
@@ -1,5 +1,5 @@
 -- Quest 28102 - Fight the power / Add Shadowstalkers to kill count
-UPDATE creature_template SET KillCredit1=7106 WHERE entry=7109;
+UPDATE creature_template SET KillCredit1=7106 WHERE entry=7110;
 
 -- Quest 28460 - Threat of the Winterspring / Update creature kill credit
-UPDATE creature_template SET KillCredit1=48586 WHERE entry=7440 OR entry=7441 OR entry=7442 OR entry=10738;
+UPDATE creature_template SET KillCredit1=48586 WHERE entry IN (7440,7441,7442,10738);
