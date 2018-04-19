1e85fb90a5da02101fcedac2f1f4d65f54deca0a
Steve <38110702+Snuffleclaws@users.noreply.github.com>
2018-04-18 21:19:43 -0400

    Update 2018_04_18_04_world.sql

--- sql/updates/world/2018_04_18_04_world.sql
+++ sql/updates/world/2018_04_18_04_world.sql
@@ -1,1 +1,1 @@
-UPDATE creature_template SET gossip_menu_id=0,ScriptName='npc_fidellio_40552' WHERE entry=40553;
+UPDATE creature_template SET gossip_menu_id=0,ScriptName='npc_fidellio_40553' WHERE entry=40553;
