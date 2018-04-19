8a627651ebf6b82f6326da7a233c1e76b8781c44
Steve <38110702+Snuffleclaws@users.noreply.github.com>
2018-04-19 00:54:16 -0400

    Update 2018_04_18_00_world.sql

--- sql/updates/world/2018_04_18_00_world.sql
+++ sql/updates/world/2018_04_18_00_world.sql
@@ -1,2 +1,2 @@
 -- Quest 9918 - Not on my watch --
-UPDATE creature_template SET AIName='',ScriptName='npc_lump_18354' WHERE entry=18351;
+UPDATE creature_template SET AIName='',ScriptName='npc_lump_18351' WHERE entry=18351;
